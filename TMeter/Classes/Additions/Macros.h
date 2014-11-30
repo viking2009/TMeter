6367d60e6941bc889df995d278fd0f059b6535d1
Mykola Vyshynskyi <viking2009@ua.fm>
2014-11-30 17:36:11 +0200

    Task #122 Resources
    - main page resources

---
+++ TMeter/Classes/Additions/Macros.h
@@ -0,0 +1,13 @@
+//
+//  Macros.h
+//  TMeter
+//
+//  Created by Mykola Vyshynskyi on 30.11.14.
+//  Copyright (c) 2014 Mykola Vyshynskyi. All rights reserved.
+//
+
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+
+#define RGB(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
+#define RGBA(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
+
